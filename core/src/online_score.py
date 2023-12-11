51403473e9c415ebefdab15e8f14cd0de77efc9a
Nicholas Moore <nfmoore@outlook.com>
2023-12-11 11:59:51 +1100

    Add native data collection to deployment

--- core/src/online_score.py
+++ core/src/online_score.py
@@ -7,21 +7,28 @@ from typing import Dict, List
 
 import mlflow
 import pandas as pd
+from azureml.ai.monitoring import Collector
 from constants import INPUT_SAMPLE, OUTPUT_SAMPLE
-from inference_schema.parameter_types.standard_py_parameter_type import (
-    StandardPythonParameterType,
-)
+from inference_schema.parameter_types.standard_py_parameter_type import \
+    StandardPythonParameterType
 from inference_schema.schema_decorators import input_schema, output_schema
 
 # define global variables
 SERVICE_NAME = None
 MODEL = None
 LOGGER = logging.getLogger("root")
-
+INPUTS_COLLECTOR = None
+OUTPUTS_COLLECTOR = None
+INPUTS_OUTPUTS_COLLECTOR = None
 
 def init() -> None:
     """Startup event handler to load an MLFLow model."""
-    global SERVICE_NAME, MODEL
+    global SERVICE_NAME, MODEL, INPUTS_COLLECTOR, OUTPUTS_COLLECTOR, INPUTS_OUTPUTS_COLLECTOR
+
+    # instantiate collectors
+    INPUTS_COLLECTOR = Collector(name="model_inputs")
+    OUTPUTS_COLLECTOR = Collector(name="model_outputs")
+    INPUTS_OUTPUTS_COLLECTOR = Collector(name="model_inputs_outputs")
 
     # Load MLFlow model
     SERVICE_NAME = "online/" + os.getenv("AZUREML_MODEL_DIR").split("/", 4)[-1]
@@ -44,14 +51,17 @@ def run(data: List[Dict]) -> str:
     """Perform scoring for every invocation of the endpoint"""
 
     try:
-        # Define UUID for the request
-        request_id = uuid.uuid4().hex
-
         # Append datetime column to predictions
         input_df = pd.DataFrame(data)
 
         # Preprocess payload and get model prediction
         model_output = MODEL.predict_proba(input_df)[:, 1].tolist()
+        output_df = pd.DataFrame(model_output, columns=["predictions"])
+
+        # --- Custom Monitoring / Data Collection ---
+
+        # Define UUID for the request
+        request_id = uuid.uuid4().hex
 
         # Log input data
         LOGGER.info(
@@ -79,6 +89,24 @@ def run(data: List[Dict]) -> str:
 
         # Make response payload
         response_payload = json.dumps({"predictions": model_output})
+
+        # ----------------------------------
+
+        # --- Azure ML Native Data Collection ---
+
+        # collect inputs data
+        context = INPUTS_COLLECTOR.collect(input_df)
+
+        # collect outputs data
+        OUTPUTS_COLLECTOR.collect(output_df, context)
+
+        # create a dataframe with inputs/outputs joined - this creates a URI folder (not mltable)
+        input_output_df = input_df.join(output_df)
+
+        # collect both your inputs and output
+        INPUTS_OUTPUTS_COLLECTOR.collect(input_output_df, context)
+
+        # ----------------------------------
 
         return response_payload
 
